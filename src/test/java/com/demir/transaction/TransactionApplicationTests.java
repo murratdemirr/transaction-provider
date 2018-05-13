d0fef29bc458c1e81be461e7113951fe2a9e354f
muratdemir <murrat.demirr@gmail.com>
2018-05-14 01:14:07 +0300

    almost done is not done

--- src/test/java/com/demir/transaction/TransactionApplicationTests.java
+++ src/test/java/com/demir/transaction/TransactionApplicationTests.java
@@ -2,6 +2,7 @@ package com.demir.transaction;
 
 import com.demir.transaction.control.TransactionService;
 import com.demir.transaction.entity.Transaction;
+import com.demir.transaction.entity.TransactionStatistics;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -12,7 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.inject.Inject;
 
@@ -36,6 +36,20 @@ public class TransactionApplicationTests {
         } catch (ExpiredTimestampException ex) {
 
         }
+    }
+
+    @Test
+    public void commitAndCheckTest() throws ExpiredTimestampException{
+        transactionService.commit(new Transaction(Double.valueOf(10), System.currentTimeMillis()));
+        transactionService.commit(new Transaction(Double.valueOf(20), System.currentTimeMillis()));
+        transactionService.commit(new Transaction(Double.valueOf(30), System.currentTimeMillis()));
+        transactionService.commit(new Transaction(Double.valueOf(40), System.currentTimeMillis()));
+        TransactionStatistics statistics = transactionService.fetchStatistics();
+        Assert.assertTrue(statistics.getCount().intValue() == 4);
+        Assert.assertTrue(statistics.getMax().doubleValue() == Double.valueOf(40).doubleValue());
+        Assert.assertTrue(statistics.getMin().doubleValue() == Double.valueOf(10).doubleValue());
+        Assert.assertTrue(statistics.getSum().doubleValue() == Double.valueOf(100).doubleValue());
+        Assert.assertTrue(statistics.getAvg().doubleValue() == Double.valueOf(25).doubleValue());
     }
 
 }
